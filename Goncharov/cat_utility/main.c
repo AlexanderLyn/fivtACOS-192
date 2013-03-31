7cd459c9c1f67b196ea27b53e242d1e98472375a
x401om <x401om@gmail.com>
2013-03-31 22:49:34 +0400

    cat and ls code improvements

--- Goncharov/cat_utility/main.c
+++ Goncharov/cat_utility/main.c
@@ -1,24 +1,19 @@
 //
 //  main.c
-//  utility_ls
+//  utility_cat
 //
 //  Created by Alexey Goncharov on 31.03.13.
 //  Copyright (c) 2013 DIHT. All rights reserved.
 //
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
-#include <glob.h>
-#include <dirent.h>
-
 
 int main(int argc, const char * argv[])
 {
-
-  DIR *dpdf;
-  struct dirent *epdf;
-  int i = 0;
   char *nPath;
   if (argc > 1) {
     char *dirName = (char *)argv[1];
@@ -26,21 +21,20 @@ int main(int argc, const char * argv[])
     nPath = malloc((2 + length)*sizeof(char));
     strcpy(nPath, "./");
     strcat(nPath, dirName);
-    dpdf = opendir(nPath);
   } else {
-    dpdf = opendir("./");
+    printf("no file to print");
+    return 0;
   }
-  if (dpdf != NULL){
-    while ((epdf = readdir(dpdf))){
-      char *nameOfFile = epdf->d_name;
-      if (nameOfFile[0] != '.') {
-        printf("%s\n", nameOfFile);
-        ++i;
-      }
+  FILE *file = fopen(nPath, "r");
+  if (file != NULL) {
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, file)) != -1) {
+      printf("%s", line);
     }
-  }
-  if (i == 0) {
-    printf("no files in such directory or directory doesn't exists");
+  } else {
+    printf("error while reading file");
   }
   return 0;
 }
