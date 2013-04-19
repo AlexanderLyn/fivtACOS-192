be5b46a36295627c82f0f391f9d583547dbce1db
Niggin <rubik303@mail.ru>
2013-04-19 12:26:41 +0300

    working pthread

--- Popov/pthread.c
+++ Popov/pthread.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #define kStep 1000
+pthread_mutex_t lock1,lock2;
 
 
 void* threadFunc(void* ar)
@@ -9,13 +10,17 @@ void* threadFunc(void* ar)
 	long long* arg = (long long*)ar;
 	while(1)//(arg[1]<arg[2])
 	{
+		pthread_mutex_lock(&lock1);
 		long long t = arg[1]++,i,sum=0;
+		pthread_mutex_unlock(&lock1);
 		for(i=t*kStep;i<(t+1)*kStep && i<=arg[3];++i)
 		{
 			sum+=i;
 		}
 		//printf("%lld\n",sum);
+		pthread_mutex_lock(&lock2);
 		arg[0]+=sum;
+		pthread_mutex_unlock(&lock2);
 		if(i >= arg[3]) return NULL;
 	}
 	return NULL;
@@ -38,6 +43,8 @@ int main(int argc, char** argv)
 	arg[1] = 0;
 	arg[2] = N/kStep;
 	arg[3] = N;
+	pthread_mutex_init(&lock1,NULL);
+	pthread_mutex_init(&lock2,NULL);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	int i;
 	for(i=0;i<K;++i) pthread_create(&tred[i],&attr,threadFunc,arg);
